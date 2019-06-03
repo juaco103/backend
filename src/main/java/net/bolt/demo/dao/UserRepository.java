2b36a7b246c90624fa490ce8540d0938f58b7ffc
Joaco <juaco103@gmail.com>
2019-06-03 18:43:14 -0500

    Method getUsers created

--- src/main/java/net/bolt/demo/dao/UserRepository.java
+++ src/main/java/net/bolt/demo/dao/UserRepository.java
@@ -1,5 +1,7 @@
 package net.bolt.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.bolt.demo.model.User;
--- src/main/java/net/bolt/demo/service/UserService.java
+++ src/main/java/net/bolt/demo/service/UserService.java
@@ -1,9 +1,13 @@
 package net.bolt.demo.service;
 
+import java.util.List;
+
 import net.bolt.demo.model.User;
 
 public interface UserService {
 
 	User save(User user);
+
+	List<User> findAll();
 
 }
