57d327ef7e28dae7d3ec400eb1da0bbe1e1e3ed5
Joaco <juaco103@gmail.com>
2019-06-03 20:16:06 -0500

    Delete method created

--- src/main/java/net/bolt/demo/service/UserService.java
+++ src/main/java/net/bolt/demo/service/UserService.java
@@ -8,6 +8,15 @@ public interface UserService {
 
 	User save(User user);
 
+	/**
+	 * Recupera la lista de usuarios
+	 * @return lista de usuarios
+	 */
 	List<User> findAll();
+	/**
+	 * Elimina un usuario con el id recibido
+	 * @param id
+	 */
+	void deleteUser(Long id);
 
 }
